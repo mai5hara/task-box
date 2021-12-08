a0d4f784140dacbe34783c21e88cf40f006a7268
mai5hara <ishiyakiimo89@gmail.com>
2021-12-08 10:29:03 -0800

    add online user component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 import './App.css';
 
 function App() {
@@ -44,6 +45,7 @@ function App() {
               />
             </Routes>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
